c37b69037329678cc1b1ce7e269e34f3e09677b5
jhonreimons123 <jhonreimons90@gmail.com>
2025-10-05 15:38:02 +0700

    update CORS

--- src/main/java/com/diangraha_backend/diangraha_backend/controller/AuthController.java
+++ src/main/java/com/diangraha_backend/diangraha_backend/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class AuthController {
 
     private final AuthService authService;
