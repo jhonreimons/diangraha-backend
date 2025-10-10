67eeeb3a912e5b06c6b17c2b86c4836f1cc32df2
jhonreimons123 <jhonreimons90@gmail.com>
2025-10-10 18:11:14 +0700

    Update CORS

--- src/main/java/com/diangraha_backend/diangraha_backend/config/WebConfig.java
+++ src/main/java/com/diangraha_backend/diangraha_backend/config/WebConfig.java
@@ -20,12 +20,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:" + location);
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
 }
