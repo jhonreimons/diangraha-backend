e434772de6dc1e404cb88b1f021f81c1da2d8ac0
jhonreimons123 <jhonreimons90@gmail.com>
2025-10-11 12:37:30 +0700

    Solve swager permission atuh

--- src/main/java/com/diangraha_backend/diangraha_backend/config/SwaggerConfig.java
+++ src/main/java/com/diangraha_backend/diangraha_backend/config/SwaggerConfig.java
@@ -1,21 +1,37 @@
 package com.diangraha_backend.diangraha_backend.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme; // <--- biarkan ini
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.servers.Server;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new io.swagger.v3.oas.models.security.SecurityScheme()
+                                        .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
                 .info(new Info()
                         .title("Diangraha Backend API")
                         .version("1.0.0")
@@ -24,15 +40,9 @@ public class SwaggerConfig {
                                 .name("Diangraha Team")
                                 .email("support@diangraha.com")))
                 .servers(List.of(
-                        new Server()
-                                .url("http://103.103.20.23:8080")
-                                .description("Production Server"),
-                        new Server()
-                                .url("http://localhost:8080")
-                                .description("Local Server"),
-                        new Server()
-                                .url("/")
-                                .description("Current Server")
+                        new Server().url("http://103.103.20.23:8080").description("Production Server"),
+                        new Server().url("http://localhost:8080").description("Local Server"),
+                        new Server().url("/").description("Current Server")
                 ));
     }
 }
